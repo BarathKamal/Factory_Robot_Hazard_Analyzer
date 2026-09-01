c5ea8ba88209849c57137bb8c543478af04a4ee0
BarathKamal <barathk1502@gmail.com>
2026-09-01 15:36:03 +0530

    Barath:Use Case 2: Accept Robot Hazard Inputs completed

--- src/main/java/com/bridgelabz/FactoryRobotHazardAnalyzer.java
+++ src/main/java/com/bridgelabz/FactoryRobotHazardAnalyzer.java
@@ -1,8 +1,25 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 // UC1: Display Static Hazard Message
 public class FactoryRobotHazardAnalyzer {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Factory Robot Hazard Analyzer");
+
+        // UC2: Accept Robot Hazard Inputs
+        System.out.println("Enter arm precision (0.0 - 1.0): ");
+        double armPrecision = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Enter Worker Density (1 - 20): ");
+        int workerDensity = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter Machinery State (Worn/Faulty/Critical): ");
+        String machineryState = scanner.nextLine();
+
+        scanner.close();
     }
 }
