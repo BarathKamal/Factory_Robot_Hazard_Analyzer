db804bcf2cbe5e99d6931e6c28604a7335ebcc04
BarathKamal <barathk1502@gmail.com>
2026-09-01 15:10:29 +0530

    Barath:Use Case 1: Display Static Hazard Message completed

--- src/main/java/com/bridgelabz/FactoryRobotHazardAnalyzer.java
+++ src/main/java/com/bridgelabz/FactoryRobotHazardAnalyzer.java
@@ -1,4 +1,8 @@
 package com.bridgelabz;
 
+// UC1: Display Static Hazard Message
 public class FactoryRobotHazardAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Factory Robot Hazard Analyzer");
+    }
 }
