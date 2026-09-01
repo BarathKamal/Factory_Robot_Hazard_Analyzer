d4f23589e21c3eea9cf12cd3c2df271b9e3823ff
BarathKamal <barathk1502@gmail.com>
2026-09-01 16:16:53 +0530

    Barath:Use Case 3: Calculate Hazard Risk Score completed

--- src/main/java/com/bridgelabz/FactoryRobotHazardAnalyzer.java
+++ src/main/java/com/bridgelabz/FactoryRobotHazardAnalyzer.java
@@ -2,21 +2,52 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
+/*
+Program that
+1. Contains the already implemented Use Cases UC1 and UC2
+2. Implements UC3 by calculating the Hazard Risk Factor and returning it through a calculateHazardRisk() method
+without validation
+ */
+
 // UC1: Display Static Hazard Message
 public class FactoryRobotHazardAnalyzer {
+
+    // UC3: Calculate Hazard Risk Score (No Validation)
+    public double calculateHazardRisk(double armPrecision, int workerDensity, String machineryState) {
+        double machineRiskFactor = 0.0;
+
+        // Assigning machineryRiskFactor to respective machineryState
+        if (machineryState.equals("Worn")) {
+            machineRiskFactor = 1.3;
+        } else if (machineryState.equals("Faulty")) {
+            machineRiskFactor = 2.0;
+        } else if (machineryState.equals("Critical")) {
+            machineRiskFactor = 3.0;
+        }
+
+        // hazardRiskScore formula
+        double hazardRiskScore = ((1.0 - armPrecision) * 15.0) + (workerDensity * machineRiskFactor);
+
+        return hazardRiskScore;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Factory Robot Hazard Analyzer");
 
         // UC2: Accept Robot Hazard Inputs
+
+        // armPrecision input
         System.out.println("Enter arm precision (0.0 - 1.0): ");
         double armPrecision = scanner.nextDouble();
         scanner.nextLine();
 
+        // workerDensity input
         System.out.println("Enter Worker Density (1 - 20): ");
         int workerDensity = scanner.nextInt();
         scanner.nextLine();
 
+        // machineryState input
         System.out.println("Enter Machinery State (Worn/Faulty/Critical): ");
         String machineryState = scanner.nextLine();
 
