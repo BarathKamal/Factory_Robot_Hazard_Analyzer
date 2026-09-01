2933b24f618d9775a2b050f60cad9b1c3c3685a2
BarathKamal <barathk1502@gmail.com>
2026-09-01 17:16:56 +0530

    Barath:Use Case 4:Introduce Validation Using Conditional Logic completed

--- src/main/java/com/bridgelabz/FactoryRobotHazardAnalyzer.java
+++ src/main/java/com/bridgelabz/FactoryRobotHazardAnalyzer.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 /*
-Program that implements UC3 by calculating the Hazard Risk Factor without validation
+Program that implements UC4 by validating the inputs using if-else conditional statements
  */
 
 public class FactoryRobotHazardAnalyzer {
@@ -29,21 +29,39 @@ public class FactoryRobotHazardAnalyzer {
         System.out.println("Enter Machinery State (Worn/Faulty/Critical): ");
         String machineryState = scanner.nextLine();
 
-        // UC3: Calculate Hazard Risk Score (No Validation)
-        double machineRiskFactor = 0.0;
-
-        // Assigning machineryRiskFactor to respective machineryState
-        if (machineryState.equals("Worn")) {
-            machineRiskFactor = 1.3;
-        } else if (machineryState.equals("Faulty")) {
-            machineRiskFactor = 2.0;
-        } else if (machineryState.equals("Critical")) {
-            machineRiskFactor = 3.0;
+        // UC4: Introduce Validation Using Conditional Logic
+        if (armPrecision < 0 || armPrecision > 1) {
+            System.out.println("Invalid Arm Precision Value");
         }
+        else if (workerDensity < 0 ||workerDensity > 20) {
+            System.out.println("Invalid Worker Density Value");
+        }
+        else if (!(machineryState.equals("Worn") ||
+                machineryState.equals("Faulty") ||
+                machineryState.equals("Critical"))) {
+            System.out.println("Invalid Machinery State");
+        }
+        else {
 
-        // hazardRiskScore formula
-        double hazardRiskScore = ((1.0 - armPrecision) * 15.0) + (workerDensity * machineRiskFactor);
+            // UC3: Calculate Hazard Risk Score (No Validation)
+            double machineRiskFactor = 0.0;
+
+            // Assigning machineryRiskFactor to respective machineryState
+            if (machineryState.equals("Worn")) {
+                machineRiskFactor = 1.3;
+            } else if (machineryState.equals("Faulty")) {
+                machineRiskFactor = 2.0;
+            } else if (machineryState.equals("Critical")) {
+                machineRiskFactor = 3.0;
+            }
+
+            // hazardRiskScore formula
+            double hazardRiskScore = ((1.0 - armPrecision) * 15.0) + (workerDensity * machineRiskFactor);
+            System.out.println("Hazard Risk Score: " + hazardRiskScore);
+        }
 
         scanner.close();
     }
 }
+
+
